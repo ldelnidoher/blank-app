de4d131596adc8d4f305d70b90ec3dcfcbec0975
ldelnidoher <lucia.delnido@ua.es>
2025-07-11 12:40:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,20 +5,19 @@ custom_css = """
 <style>
     /* Custom style for the active tab */
     .stTabs > .tablist > .react-tabs__tab--selected {
-        background-color: #0e1117;
+        background-color: #f505cd;
         color: #ffffff;
         font-family: 'Courier New', Courier, monospace;
     }
     /* Custom style for all tabs */
     .stTabs > .tablist > .react-tabs__tab {
         background-color: #e8e8e8;
-        color: #4f4f4f;
+        color: #f50202;
         font-family: 'Courier New', Courier, monospace;
     }
 </style>
 """
 
-st.markdown(custom_css, unsafe_allow_html=True)
 
 # Example usage of tabs
 tab1, tab2 = st.tabs(["Tab 1", "Tab 2"])
@@ -28,3 +27,6 @@ with tab1:
 
 with tab2:
     st.write("Content in tab 2")
+
+
+st.markdown(custom_css, unsafe_allow_html=True)
